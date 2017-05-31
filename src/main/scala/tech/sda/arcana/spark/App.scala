c0e6382600ab16979129dd3e73b9e987ce423ae5
Ali Denno <aliedenno@gmail.com>
2017-05-31 12:06:46 +0200

    Update App.scala, Points to local file

--- src/main/scala/tech/sda/arcana/spark/App.scala
+++ src/main/scala/tech/sda/arcana/spark/App.scala
@@ -8,23 +8,10 @@ import net.sansa_stack.rdf.spark.io.NTripleReader
 object App {
 
   def main(args: Array[String]) = {
-    if (args.length < 1) {
-      System.err.println(
-        "Usage: Triple reader <input>")
-      System.exit(1)
-    }
-    val input = args(0)
-    val optionsList = args.drop(1).map { arg =>
-      arg.dropWhile(_ == '-').split('=') match {
-        case Array(opt, v) => (opt -> v)
-        case _             => throw new IllegalArgumentException("Invalid argument: " + arg)
-      }
-    }
-    val options = mutable.Map(optionsList: _*)
 
-    options.foreach {
-      case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
-    }
+    //val input = args(0)
+    val input = "src/main/resources/rdf.nt"
+
     println("======================================")
     println("|        Triple reader example       |")
     println("======================================")
