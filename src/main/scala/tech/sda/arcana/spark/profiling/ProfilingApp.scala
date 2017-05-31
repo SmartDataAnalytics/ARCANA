83112348ddfa29e59d595926c1ff3b9444794d81
Ali Denno <aliedenno@gmail.com>
2017-05-31 15:03:54 -0400

    Add ProfilingApp.scala

---
+++ src/main/scala/tech/sda/arcana/spark/profiling/ProfilingApp.scala
@@ -0,0 +1,20 @@
+package tech.sda.arcana.spark.profiling
+
+import java.io.File
+import scala.collection.mutable
+import org.apache.spark.sql.SparkSession
+import net.sansa_stack.rdf.spark.io.NTripleReader
+
+object App {
+
+  def main(args: Array[String]) = {
+
+    println("==========================")
+    println("|        Profiling       |")
+    println("==========================")
+
+
+
+  }
+
+}
