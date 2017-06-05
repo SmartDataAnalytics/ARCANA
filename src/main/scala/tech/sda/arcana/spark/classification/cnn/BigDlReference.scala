dad36734e6d85171f82ca52414d22b82a0ef10a8
mhddenno <dennokhmhd@gmail.com>
2017-06-05 07:22:57 +0200

    adding test object for bigdl

--- src/main/scala/tech/sda/arcana/spark/classification/cnn/BigDlReference.scala
+++ src/main/scala/tech/sda/arcana/spark/classification/cnn/BigDlReference.scala
@@ -1,4 +1,5 @@
 package tech.sda.arcana.spark.classification.cnn
+/*
 //1
 import com.intel.analytics.bigdl.tensor.Tensor
 //2
@@ -6,11 +7,44 @@ import com.intel.analytics.bigdl.utils.T
 //3
 import com.intel.analytics.bigdl.numeric.NumericFloat
 import com.intel.analytics.bigdl.nn._
+*/
+/*
+import com.intel.analytics.bigdl._
+import com.intel.analytics.bigdl.utils.Engine
+
+import org.apache.spark.SparkContext
+*/
+
+import com.intel.analytics.bigdl.utils.Engine
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
 
 object BigDlReference {
   def main(args:Array[String]){
-  
+
     
+    val conf = Engine.createSparkConf()
+     .setAppName("Train Lenet on MNIST")
+     .set("spark.task.maxFailures", "1")
+     .setMaster("local")
+   val sc = new SparkContext(conf)
+   Engine.init
+
+   println(s" the Engine status is ${Engine.init}")
+   println("Hello")
+    
+    
+    
+    
+    
+/*
+   val conf = Engine.createSparkConf()
+   val sc = new SparkContext(conf)
+   Engine.init
+    */
+    
+    
+    /*
   //1
   val tensor = Tensor[Float](2, 3)
   println(tensor)
@@ -21,5 +55,6 @@ object BigDlReference {
   val f = Linear(3,4)
   val weights=f.weight
   println(weights)
+  * */
   }
 }
