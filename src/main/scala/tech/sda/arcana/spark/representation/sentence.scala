f1584ba14de01b623b8e00ee7a7a197b44e383a6
mhddenno <dennokhmhd@gmail.com>
2017-06-07 11:32:11 +0200

    Define the structure of many representations' approaches with respect to a word and a sentence

---
+++ src/main/scala/tech/sda/arcana/spark/representation/sentence.scala
@@ -0,0 +1,8 @@
+package tech.sda.arcana.spark.representation
+
+object sentence {
+  //choose the way of representation a sentence possible choices:
+  //add words' vectors together
+  //put the vectors of the words beside each other
+  //subtract words' vectors together
+}
