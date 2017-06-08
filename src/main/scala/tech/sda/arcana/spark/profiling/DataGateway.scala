88d2d1c7789c25dbd7be0dfb0a91034d6a6eb9b7
Ali Denno <aliedenno@gmail.com>
2017-06-08 15:47:47 -0400

    Update DataGateway.scala
    Writing file to the db

--- src/main/scala/tech/sda/arcana/spark/profiling/DataGateway.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/DataGateway.scala
@@ -5,10 +5,11 @@ import com.mongodb
 import org.bson.Document
 import com.mongodb.spark.config._
 import org.apache.spark.SparkContext
+import play.api.libs.json._
+import org.bson.Document
+import scala.util.parsing.json._
 
 object AppDBM {
-
-
   def main(args: Array[String]) = {
     println("===================X===================")
     println("===================")
@@ -33,6 +34,45 @@ object AppDBM {
     println("===================Sc===================")
     val configMap:Map[String, String] = spark.conf.getAll
     println(configMap)
+    println("===================Writing===================")
+    
+    
+    val result = JSON.parseFull("""
+      {"name": "Naoki",  "lang": ["Java", "Scala"]}
+    """)
+    
+    var reso="""
+      {"name": "Naoki",  "lang": ["Java", "Scala"]}
+    """
+    /*
+    val jsonStr = 
+    "{word:" +"\"troops\","+
+    "rank:"+  "\"4\","+
+    "rsc:[" +
+        "\"http://dbpedia.org/resource/Territorial_Troops1\"," +
+        "\"http://dbpedia.org/resource/Territorial_Troops2\"," +
+        "\"http://dbpedia.org/resource/Territorial_Troops3\""+
+    "]}"
+    */
+    
+    //val jsonStr = """{"word":"sroops","rank":"3","rsc":["http://dbpedia.org/resource/Territorial_Troops1","http://dbpedia.org/resource/Territorial_Troops2"]}"""    
+            
+    //println(jsonStr)
+
+    //val TRDD = sc.parallelize(result:: Nil)
+    //val JSRDD = sc.parallelize(Seq(jsonStr))
+    """{"name": "Naoki",  "lang": ["Java", "Scala"]}"""
+    
+
+    val jsonStr = """{ "metadata": { "key": 84896, "value": 54 }}"""
+    val rddw = sc.parallelize(Seq(jsonStr))
+  //  val documents = Document.parse(reso)
+    //val rddw = sc.parallelize(Seq((1, "Spark"), (2, "Databricks")))
+    
+    val documents = sc.parallelize((1 to 2).map(i => Document.parse(s"""{"word":"sroops","rank":"3","rsc":["http://dbpedia.org/resource/Territorial_Troops1","http://dbpedia.org/resource/Territorial_Troops2"]}""")))    
+    
+    MongoSpark.save(documents)          
+    println("===================Closing Writing===================")
     println("===================LOADING===================")
     val rdd = MongoSpark.load(spark)
     //val rdd = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/myDBN.military")))
@@ -62,9 +102,11 @@ object AppDBM {
     //println(configMap2)
     //spark.sparkContext.loadFromMongoDB() // Uses the SparkConf for configuration
     //spark.sparkContext.loadFromMongoDB(ReadConfig(Map("spark.mongodb.output.uri" -> "myDBN.military"))) // Uses the ReadConfig
-    println("===================ter===================")
+    println("===================ChangeCollection===================")
     val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/myDBN.neuclear")))
     rdd2.toDF().select("word").show()
+    
+
     println("===================CLOSING===================") 
     spark.stop()
   }
