e6c26de5b286d7cb638e7d4d3f633c951648b094
Ali Denno <aliedenno@gmail.com>
2017-06-14 15:46:51 -0400

    Add RDFGateway.scala
    A gateway to deal with RDF Data

---
+++ src/main/scala/tech/sda/arcana/spark/profiling/RDFGateway.scala
@@ -0,0 +1,20 @@
+package tech.sda.arcana.spark.profiling
+
+import java.io.File
+import scala.collection.mutable
+import org.apache.spark.sql.SparkSession
+import net.sansa_stack.rdf.spark.io.NTripleReader
+
+object RDFApp {
+
+  def main(args: Array[String]) = {
+
+    println("============================")
+    println("|        RDF Gateway       |")
+    println("============================")
+
+   
+    
+  }
+
+}
