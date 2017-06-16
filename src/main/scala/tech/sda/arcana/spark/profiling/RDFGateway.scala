4f180772b28ea36f6439d162c2bf18e9c5ed0daa
Ali Denno <aliedenno@gmail.com>
2017-06-15 22:13:15 -0400

    Update RDFGateway.scala
    make a basic interacton with RDF data that loads the data into a dataframe by using a mapper

--- src/main/scala/tech/sda/arcana/spark/profiling/RDFGateway.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/RDFGateway.scala
@@ -4,17 +4,53 @@ import java.io.File
 import scala.collection.mutable
 import org.apache.spark.sql.SparkSession
 import net.sansa_stack.rdf.spark.io.NTripleReader
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import org.apache.spark.sql.SparkSession;
+
 
 object RDFApp {
-
+  //rdf subject predicate object
+  case class Triple(Subject:String, Predicate:String, Object:String)
+  
+  case class Person(ID:Int, name:String, age:Int, numFriends:Int)
+  
+  def mapperRDF(line:String): Triple = {
+    val fields = line.split(" ")  
+    
+    val triple:Triple = Triple(fields(0), fields(1), fields(2))
+    return triple
+  }
+  
+  def mapper(line:String): Person = {
+    val fields = line.split(',')  
+    
+    val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
+    return person
+  }
+  
   def main(args: Array[String]) = {
-
+  
     println("============================")
     println("|        RDF Gateway       |")
     println("============================")
-
-   
+    val input = "src/main/resources/rdf.nt"
     
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("RDFApp")
+      .master("local[*]")
+      .getOrCreate()
+    
+   import spark.implicits._ 
+    val lines = spark.sparkContext.textFile(input)
+    val triples = lines.map(mapperRDF).toDS().cache()
+
+
+   triples.select("Object").show()
+   //people.foreach(println)
+   spark.stop()
   }
 
 }
