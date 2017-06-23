16ee320c595a94f034ef0c4c58bf8518fb740a33
Ali Denno <aliedenno@gmail.com>
2017-06-23 15:41:56 -0400

    Create Object that implements functions from StanfordCoreNLP
    Functions that build on what Gezim's had. Tokenizer was implemented

--- src/main/scala/tech/sda/arcana/spark/profiling/StanfordCoreNLP.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/StanfordCoreNLP.scala
@@ -4,9 +4,11 @@ import java.io.File
 import scala.collection.mutable
 import org.apache.spark.sql.SparkSession
 import net.sansa_stack.rdf.spark.io.NTripleReader
-
+import java.io.StringReader
 import java.util.Properties
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation
 import edu.stanford.nlp.pipeline.{ Annotation, StanfordCoreNLP }
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentClass
@@ -14,7 +16,22 @@ import edu.stanford.nlp.util.CoreMap
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation
 import edu.stanford.nlp.ling.CoreAnnotations
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
 
+
+import java.util.Properties
+import scala.collection.JavaConversions._ 
+import scala.collection.immutable.ListMap
+import scala.io.Source
+
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation
+import edu.stanford.nlp.ling.CoreLabel
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
 object Stanford {
   
   val properties = new Properties()
@@ -22,7 +39,7 @@ object Stanford {
   properties.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, sentiment");
   properties.setProperty("ssplit.isOneSentence", "true")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(properties)
-
+  val coreNLP = new StanfordCoreNLP(properties)
   def Sentiment(sentence: String): String = {
 
     // create an empty Annotation just with the given text
@@ -50,11 +67,50 @@ object Stanford {
 
     dependencies.toString()
 }
+  
+  // This one is hosted on the sit and works nicely 
+  def PTBTokenizer(sentence: String): PTBTokenizer[CoreLabel] = {
 
-  def main(args: Array[String]) = {
-    
-    val test = Sentiment("Milk")
-    println(test)
-  }
+    val ptbt: PTBTokenizer[CoreLabel] = new PTBTokenizer[CoreLabel](new StringReader("HEY THERE GUYS"), new CoreLabelTokenFactory(), "")
 
+    ptbt
 }
+  
+  // This one also works nicely and you can add some more functionality, the normalizeToken compliment this one 
+  def tokenize(s: String)  = { 
+
+            val annotation = new Annotation(s)
+            coreNLP.annotate(annotation)
+            annotation.get(classOf[TokensAnnotation]).map(_.toString)
+                       
+          }
+  def normalizeToken(t: String) = {
+    val ts = t.toLowerCase
+    val num = "[0-9]+[,0-9]*".r
+    ts match {
+      case num() => "NUMBER"
+      case _ => ts
+    }
+}
+  def main(args: Array[String]) = {
+ 
+     //val dp: DocumentPreprocessor = new DocumentPreprocessor("Hey let us go and play") 
+     //for (sentence <- dp) { println(sentence) }
+    
+    //val ptbt: PTBTokenizer[CoreLabel] = new PTBTokenizer[CoreLabel](new StringReader("HEY THERE GUYS"), new CoreLabelTokenFactory(), "")
+    val ptbt = PTBTokenizer("HEY THERE GUYS")
+    
+    while (ptbt.hasNext) { 
+      val label: CoreLabel = ptbt.next() 
+      println(label) }
+    
+    //val test = tokenize("Hi Hi Let's me go")
+    //   for ( x <- test ) {
+    //     println( x )
+    //  }
+    //println(test)
+    println("DON")
+  }
+}
+
+
