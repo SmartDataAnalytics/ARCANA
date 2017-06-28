fb29b9996572de2ce9506a147b333fa38ffd7dda
Ali Denno <aliedenno@gmail.com>
2017-06-28 13:53:44 -0400

    Update Category.scala
    Remove duplicate initialization of categories

--- src/main/scala/tech/sda/arcana/spark/profiling/Category.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/Category.scala
@@ -2,10 +2,5 @@ package tech.sda.arcana.spark.profiling
 
 class Category {
   var categories = Array("military", "nuclear", "terrorism", "weapon", "technology", "security", "harm", "suicide")
-  
-  //var categories = Array("military","nuclear","terrorism","weapon","technology","security","harm","suicide")
-  
-  
-  
 }
 
