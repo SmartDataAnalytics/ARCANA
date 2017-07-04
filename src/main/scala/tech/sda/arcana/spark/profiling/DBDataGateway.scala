e1ddb5aeb6f1a958ef4e7a2349311e95f0699a5b
Ali Denno <aliedenno@gmail.com>
2017-07-04 17:20:13 -0400

    Update 1.DBDataGateway and 2.pom.xml
    Update the mechanism to deal with data (1) and Add libraries for circe which help in parsing JSON files

--- src/main/scala/tech/sda/arcana/spark/profiling/DBDataGateway.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/DBDataGateway.scala
@@ -1,4 +1,8 @@
 package tech.sda.arcana.spark.profiling
+//import io.circe.jackson
+import io.circe.syntax._
+
+
 import com.mongodb.spark._
 import org.apache.spark.sql.SparkSession
 import com.mongodb
@@ -11,71 +15,54 @@ import scala.util.parsing.json._
  * An Object that is responsible for the interaction with MongoDB to store and read data
  */
 object AppDBM {
-  def main(args: Array[String]) = {
-    println("===================X===================")
-    println("===================")
-    println("|        DB       |")
-    println("===================")
     
-    val inputUri = "spark.mongodb.input.uri"  
-    val outputUri = "spark.mongodb.output.uri"
-    val conn = new DBConf()  
-    
-    val spark = SparkSession.builder()
-      .master("local")
-      .appName("MongoSparkConnectorIntro")
-      .config(inputUri, conn.host + conn.dbName + "." + conn.defaultCollection)
-      .config(outputUri, conn.host + conn.dbName + "." + conn.defaultCollection)
-      //.config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse")
-      .getOrCreate()
-    
-    val sc = spark.sparkContext
-    //println(sc)
-    
-    println("===================Sc===================")
-    val configMap:Map[String, String] = spark.conf.getAll
-    println(configMap)
-    println("===================Writing===================")
-    
+  val inputUri = "spark.mongodb.input.uri"  
+  val outputUri = "spark.mongodb.output.uri"
+  val conn = new DBConf()  
   
-    val doc = Document.parse(s"""{"word":"ALIroops","rank":"3","rsc":["http://dbpedia.org/resource/Territorial_Troops1","http://dbpedia.org/resource/Territorial_Troops2"]}""")
-    val documents = sc.parallelize(Seq(doc))
-    //val documents = sc.parallelize((1 to 2).map(i => Document.parse(s"""{"word":"sroops","rank":"3","rsc":["http://dbpedia.org/resource/Territorial_Troops1","http://dbpedia.org/resource/Territorial_Troops2"]}""")))    
-    
-    MongoSpark.save(documents)          
-    println("===================Closing Writing===================")
-    println("===================LOADING===================")
-    val rdd = MongoSpark.load(spark)
-    //val rdd = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/myDBN.military")))
+  val spark = SparkSession.builder()
+    .master("local")
+    .appName("MongoSparkConnectorIntro")
+    .config(inputUri, conn.host + conn.dbName + "." + conn.defaultCollection)
+    .config(outputUri, conn.host + conn.dbName + "." + conn.defaultCollection)
+    //.config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse") >> Windows
+    .getOrCreate()
+  
+  val sc = spark.sparkContext
+  
 
-    println("===================RDD===================")
-    /*
-    rdd.printSchema() //show schema
-    rdd.createOrReplaceTempView("info")
-    rdd.select("word").show()
-    val Ds=spark.sql("select rsc from info")
-    val results = Ds.collect()
-    results.foreach(println)
-    */
-    println("===================DF===================") 
-    val myDF=rdd.toDF()
-    myDF.show()
-    myDF.select("word").show()
-    myDF.filter(myDF("rank")<4).show
-    //myDF.groupBy("rank").count().show()
-    myDF.select(myDF("rsc"), myDF("rank")+10).show()
+  def showConfigMap(){
+      val configMap:Map[String, String] = spark.conf.getAll
+      println(configMap)
+  }  
+  
+  def writeToMongoDB(x:String){
+      val doc = Document.parse(x)
+      val documents = sc.parallelize(Seq(doc))
+      MongoSpark.save(documents) 
+  }  
+  
+  def list2StringList(x: List[String]): String = {
+    var Result = ""
+    for (ship <- x) {
+      Result=Result+'"'+ship+'"'+','
+      //println(ship)
+      }  
+    return Result
+  }
     
-     
-     println("===================Collection2===================") 
-    //spark.conf.set("spark.mongodb.input.uri", "mongodb://127.0.0.1/myDBN.neuclear")
-    // spark.conf.set("spark.mongodb.output.uri", "mongodb://127.0.0.1/myDBN.neuclear")
-    //val configMap2:Map[String, String] = spark.conf.getAll
-    //println(configMap2)
-    //spark.sparkContext.loadFromMongoDB() // Uses the SparkConf for configuration
-    //spark.sparkContext.loadFromMongoDB(ReadConfig(Map("spark.mongodb.output.uri" -> "myDBN.military"))) // Uses the ReadConfig
-    println("===================ChangeCollection===================")
-    val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> "mongodb://127.0.0.1/myDBN.neuclear")))
-    rdd2.toDF().select("word").show()
+  def main(args: Array[String]) = {
+    println("===================OPEN===================") 
+    showConfigMap()
+    
+    val word = "ALIroops"
+    val rank = "3"
+    val rsclist  = List[String]("http://dbpedia.org/resource/Territorial_Troops1", "http://dbpedia.org/resource/Territorial_Troops2", "http://dbpedia.org/resource/Territorial_Troops3","http://dbpedia.org/resource/Territorial_Troops4","http://dbpedia.org/resource/Territorial_Troops5","http://dbpedia.org/resource/Territorial_Troops6","http://dbpedia.org/resource/Territorial_Troops7")
+    //val rsc=rsclist.mkString("\"", "\",\"", "\"")
+    //val test= mapper.writeValueAsString(rsclist)
+    val rsc = rsclist.asJson
+    println(rsc)
+    //writeToMongoDB(s"""{"word":"$word","rank":"$rank","rsc":$rsc}""")
     
 
     println("===================CLOSING===================") 
