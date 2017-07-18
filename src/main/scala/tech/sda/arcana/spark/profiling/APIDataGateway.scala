d47d6434f20c31d44e899611a5891e3daf7b2dd4
Ali Denno <aliedenno@gmail.com>
2017-07-18 17:22:59 -0400

    Update APIDataGateway.scala
    Add Methods to parse JSON(Using Lift) and xml)

--- src/main/scala/tech/sda/arcana/spark/profiling/APIDataGateway.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/APIDataGateway.scala
@@ -1,7 +1,7 @@
 package tech.sda.arcana.spark.profiling
 
 //import io.circe._, io.circe.parser._
-
+import net.liftweb.json.Xml.{toJson, toXml}
 import net.liftweb.json._
 import java.io._
 import scala.util.parsing.json._
@@ -41,48 +41,63 @@ object APIData {
   // 1st way to do it
   @throws(classOf[java.io.IOException])
   def fetch(url: String) = scala.io.Source.fromURL(url).mkString
-  
-  
-  case class noun(syn: List[String],ant: List[String])
-  case class verb(syn: List[String],ant: List[String])
-  case class Obj(noun:noun,verb:verb)
 
   def getSynomyns_bighugelabs(expression:String){
+    case class noun(syn: List[String],ant: List[String])
+    case class verb(syn: List[String],ant: List[String])
+    case class Obj(noun:noun,verb:verb)
+    
     implicit val formats = net.liftweb.json.DefaultFormats
     val result = fetch(s"http://words.bighugelabs.com/api/2/fe297721a04ca9641ae3a5b1ae3033a2/$expression/json")
+    val json = parse(result)
+    val Z = json.extract[Obj] 
+    Z.noun.syn.foreach { println }
+    println("------------------------------------")
+    Z.verb.syn.foreach { println }
+  }
+    def getSynomyns_merriam(expression:String){
+    case class noun(syn: List[String],ant: List[String])
+    case class verb(syn: List[String],ant: List[String])
+    case class Obj(noun:noun,verb:verb)
+    
+    implicit val formats = net.liftweb.json.DefaultFormats
+    val result = fetch(s"http://words.bighugelabs.com/api/2/fe297721a04ca9641ae3a5b1ae3033a2/$expression/json")
+    val json = parse(result)
+    val Z = json.extract[Obj] 
+    Z.noun.syn.foreach { println }
+    println("------------------------------------")
+    Z.verb.syn.foreach { println }
+  }
+  
+  def main(args: Array[String]) = {
+
+     //getSynomyns_bighugelabs("kill")
+
+    val result2 = fetch("http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/war?key=e8c94890-746e-4df8-98da-08cdf5d84e53")
+    print(result2)
+    println("===========================================")
+    val tx = scala.xml.XML.loadString(result2)
+    val json = toJson(tx)
+    //pretty(render(json))
+    val x = <div class="content"><p>Hello</p><p>world</p></div>
+    val t = x \ "term"
+    //println(tx.child)
+    val strings = for {
+        e <- tx.child
+      
+    } println( (e \\ "syn").text)
+    //strings.foreach { println }
+    //println(strings)
+    //val x= "How+to+kill+a+person?"
+    //val result3 = fetch("https://api.uclassify.com/v1/uClassify/Sentiment/classify/?readKey=L5ZjO3PO2YlO&text="+x)
+    //println(result3)
+    
+  }
+}
+    //val jsonAst = result.parseJson // or JsonParser(source)
+    //val json = jsonAst.prettyPrint // or .compactPrint
+    //println(json)
+
     //val parsed = JSON.parseFull(result)
     //val jsonAst = result.parseJson // or JsonParser(source)
     //val json = jsonAst.prettyPrint // or .compactPrint
-    val json = parse(result)
-    val Z = json.extract[Obj] 
-     Z.verb.syn.foreach { println }
-    //println(Z.noun.syn)
-    //val nOBJ = N.extract[Obj]
-    
-    //val parseResult = parse(result)
-    //println(parseResult)
-  }
-  
-  
-  def main(args: Array[String]) = {
-  
-    println("============================")
-    println("|        API Gateway       |")
-    println("============================")
-    
-
-    getSynomyns_bighugelabs("war")
-    //println(parsed)
-    /*
-    val jsonAst = result.parseJson // or JsonParser(source)
-    val json = jsonAst.prettyPrint // or .compactPrint
-    //println(json)
-    
-    val result2 = fetch("http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/war?key=e8c94890-746e-4df8-98da-08cdf5d84e53")
-    //println(result2)
-    val x= "How+to+kill+a+person?"
-    val result3 = fetch("https://api.uclassify.com/v1/uClassify/Sentiment/classify/?readKey=L5ZjO3PO2YlO&text="+x)
-    println(result3)
-    */
-  }
-}
