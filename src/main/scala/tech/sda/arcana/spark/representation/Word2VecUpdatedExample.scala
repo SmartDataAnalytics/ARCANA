b94b0472d4aa12a2926d8568e553185786d225f3
Ali Denno <aliedenno@gmail.com>
2017-10-01 19:28:20 +0200

    Create Word2VecUpdatedExample.scala
    An updated example that showes how to transform a bag of words to feature vectors

---
+++ src/main/scala/tech/sda/arcana/spark/representation/Word2VecUpdatedExample.scala
@@ -0,0 +1,42 @@
+// scalastyle:off println
+package org.apache.spark.examples.ml
+
+// $example on$
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.Row
+// $example off$
+import org.apache.spark.sql.SparkSession
+
+object Word2VecUpdatedExample {
+    def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("Word2Vec example")
+      .getOrCreate()
+
+    // $example on$
+    // Input data: Each row is a bag of words from a sentence or document.
+    val documentDF = spark.createDataFrame(Seq(
+      "Hi I heard about Spark".split(" "),
+      "I wish Java could use case classes".split(" "),
+      "Logistic regression models are neat".split(" ")
+    ).map(Tuple1.apply)).toDF("text")
+
+    // Learn a mapping from words to Vectors.
+    val word2Vec = new Word2Vec()
+      .setInputCol("text")
+      .setOutputCol("result")
+      .setVectorSize(3)
+      .setMinCount(0)
+    val model = word2Vec.fit(documentDF)
+
+    val result = model.transform(documentDF)
+    result.collect().foreach { case Row(text: Seq[_], features: Vector) =>
+      println(s"Text: [${text.mkString(", ")}] => \nVector: $features\n") }
+    // $example off$
+
+    spark.stop()
+  }
+}
+// scalastyle:on println
