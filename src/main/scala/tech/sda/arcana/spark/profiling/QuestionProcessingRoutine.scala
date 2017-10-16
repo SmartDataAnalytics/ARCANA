baef0412b9d942b73670a09e3eb5eb4b0d21c58b
Ali Denno <aliedenno@gmail.com>
2017-10-16 16:07:30 +0200

    Update QuestionProcessingRoutine
    Add Simple tokenizing and tokenizing using Regex

--- src/main/scala/tech/sda/arcana/spark/profiling/QuestionProcessingRoutine.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/QuestionProcessingRoutine.scala
@@ -9,7 +9,17 @@ object QuestionProcessingRoutine {
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .appName("Dataset2Vec")
       .getOrCreate()
+      
+  // Simple Question Tokenizing
   def tokenizeQuestion(question: String){
+    /*
+     * in case of many question you can do:
+      val sentenceDataFrame = spark.createDataFrame(Seq(
+      (0, "Hi I heard about Spark"),
+      (1, "I wish Java could use case classes"),
+      (2, "Logistic,regression,models,are,neat")
+  	  )).toDF("id", "sentence")
+     */
     val sentenceDataFrame = spark.createDataFrame(Seq(
       (0, question)
     )).toDF("id", "sentence")
@@ -19,32 +29,25 @@ object QuestionProcessingRoutine {
     tokenized.select("sentence", "words")
         .withColumn("tokens", countTokens(col("words"))).show(false)
   }
-  def main(args: Array[String]) = {
+  // Question Tokenizing while using Regex
+  def tokenizeQuestionWithRegex(question: String){
     val sentenceDataFrame = spark.createDataFrame(Seq(
-      (0, "Hi I heard about Spark"),
-      (1, "I wish Java could use case classes"),
-      (2, "Logistic,regression,models,are,neat")
+      (0, question)
     )).toDF("id", "sentence")
-    
-    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
-   /* val regexTokenizer = new RegexTokenizer()
+    val regexTokenizer = new RegexTokenizer()
       .setInputCol("sentence")
       .setOutputCol("words")
       .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
-    */
     val countTokens = udf { (words: Seq[String]) => words.length }
-    
-    val tokenized = tokenizer.transform(sentenceDataFrame)
-    tokenized.select("sentence", "words")
-        .withColumn("tokens", countTokens(col("words"))).show(false)
-        
-    tokenizeQuestion("Hi There How are you?")    
-    
-    /*
     val regexTokenized = regexTokenizer.transform(sentenceDataFrame)
     regexTokenized.select("sentence", "words")
         .withColumn("tokens", countTokens(col("words"))).show(false)
-  	*/
-   }
+  }
   
+  def main(args: Array[String]) = {
+   
+    tokenizeQuestion("Hi There How are you?")    
+    tokenizeQuestionWithRegex("Hi There How are you?")  
+
+   } 
 }
