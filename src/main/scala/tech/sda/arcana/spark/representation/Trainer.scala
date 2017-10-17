3d452f4b34132e58f9996d72e2c0ed8bdc62417f
mhddenno <dennokhmhd@gmail.com>
2017-10-17 19:28:30 +0200

    adding the training class

--- src/main/scala/tech/sda/arcana/spark/representation/Trainer.scala
+++ src/main/scala/tech/sda/arcana/spark/representation/Trainer.scala
@@ -1,6 +1,9 @@
 package tech.sda.arcana.spark.representation
 import com.intel.analytics.bigdl.nn.ClassNLLCriterion
 import com.intel.analytics.bigdl.nn.L1Cost
+import tech.sda.arcana.spark.neuralnetwork.model.AlexNetModel
+import tech.sda.arcana.spark.neuralnetwork.model.GoogLeNetModel
+import tech.sda.arcana.spark.neuralnetwork.model.LeNet5Model
 
 class Trainer(lossfun:Int,model:Int) {
   val lossFunctions = Array(L1Cost[Float](),ClassNLLCriterion[Float]())
