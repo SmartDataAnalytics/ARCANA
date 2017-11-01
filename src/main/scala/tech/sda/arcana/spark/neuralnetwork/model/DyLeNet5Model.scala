4b9c76a8375cb6d8ccfb65c617971ca32dde9002
mhddenno <dennokhmhd@gmail.com>
2017-11-01 11:05:37 +0100

    tying to adapt the model to dynamically work on any inputs
    the inputs should have some conditions

--- src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
+++ src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
@@ -4,18 +4,25 @@ import com.intel.analytics.bigdl.nn._
 
 object DyLeNet5Model {
   
-  def build(x:Int,y:Int)={
-    val view=16*(((x-4)/2)/4)*(((y-4)/2)/4)
+  def build(Height:Int,Width:Int)={
+    if((Height<=12 && Width<=12)&&(Height%2!=0 && Width%2!=0)){
+      val message:String="error"
+      println(message)
+      message
+    }
+    
+    
+    val view:Int=16*(((Height-4)/2)-4)*(((Width-4)/2)-4)
     val LeNet5Model= new Sequential()
     // 1 input channel, 6 output channels, 5x5 convolution kernel
     LeNet5Model.add(SpatialConvolution(1, 6, 5, 5))
-    println(LeNet5Model.getParametersTable())
+
   
     
     //non-linearity (+Z)
     LeNet5Model.add(ReLU())
     
-    println("1")
+ 
     
     //A max-pooling operation that looks at 2x2 windows and finds the max.
     LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
@@ -23,12 +30,15 @@ object DyLeNet5Model {
     LeNet5Model.add(ReLU())
     LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
     
-    println("2")
+
+        println(LeNet5Model.getParametersTable())
     
+    println(((Height-4)/2)-4)
+    println(((Width-4)/2)-4)
+    println(view)
     //reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
     LeNet5Model.add(View(view))
     
-    println("3")
     
     //fully connected layer (matrix multiplication between input and weights)
     LeNet5Model.add(Linear(view,120))
@@ -37,8 +47,7 @@ object DyLeNet5Model {
     LeNet5Model.add(ReLU())
     LeNet5Model.add(Linear(84,10))
     LeNet5Model.add(LogSoftMax())
-    
-    println("3")
+   
     
     LeNet5Model
   }
