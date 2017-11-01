770bb94617d3064b2bc03ede734d5bdd736e3489
Ali Denno <aliedenno@gmail.com>
2017-11-01 21:12:23 +0100

    Update DBDataGateway
    Add URIs of categories to the DB

--- src/main/scala/tech/sda/arcana/spark/profiling/DBDataGateway.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/DBDataGateway.scala
@@ -1,7 +1,7 @@
 package tech.sda.arcana.spark.profiling
-import org.apache.spark.sql.functions.{min, max}
+import org.apache.spark.sql.functions.{ min, max }
 import org.apache.spark.sql.Row
-import org.apache.spark.sql._ 
+import org.apache.spark.sql._
 import io.circe.syntax._
 import scala.collection.JavaConverters._
 import com.mongodb.spark._
@@ -20,10 +20,10 @@ import org.bson.types.ObjectId
  * An Object that is responsible for the interaction with MongoDB to store and read data
  */
 object AppDBM {
-    
-  val inputUri = "spark.mongodb.input.uri"  
+
+  val inputUri = "spark.mongodb.input.uri"
   val outputUri = "spark.mongodb.output.uri"
-  
+
   val spark = SparkSession.builder()
     .master("local")
     .appName("MongoSparkConnector")
@@ -31,10 +31,10 @@ object AppDBM {
     .config(outputUri, DBConf.host + DBConf.dbName + "." + DBConf.defaultCollection)
     //.config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse") >> Windows
     .getOrCreate()
-  
+
   val sc = spark.sparkContext
- 
-  def writeChunkToMongoDB(collection:String){
+
+  def writeChunkToMongoDB(collection: String) {
     val docs = """
       {"name": "Bilbo Baggins", "age": 50}
       {"name": "Gandalf", "age": 1000}
@@ -46,10 +46,10 @@ object AppDBM {
       {"name": "Glóin", "age": 158}
       {"name": "Fíli", "age": 82}
       {"name": "Bombur"}"""
-      //.trim.stripMargin.split("[\\r\\n]+").toSeq
-      //println(docs)
-    
-    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+    //.trim.stripMargin.split("[\\r\\n]+").toSeq
+    //println(docs)
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val events = sc.parallelize(docs :: Nil)
 
     // read it
@@ -57,112 +57,108 @@ object AppDBM {
 
     //df.show
 
-     import sqlContext.implicits._
-     
-     MongoSpark.save(df.write.option("collection", "DFClub").mode("append")) 
+    import sqlContext.implicits._
+
+    MongoSpark.save(df.write.option("collection", "DFClub").mode("append"))
     /*val json: JsValue = Json.parse(docs)
 
 
       val rdd = sc.parallelize(jsonStr::Nil);
       var df = sqlContext.read.json(rdd);
       df.printSchema()
-    
-    
+
+
     val df = spark.read.format("json").json(docs)
     df.show()
     */
-    
-   
-    
+
     val j = sc.parallelize(docs)
-    
+
     //val documents = sc.parallelize(docs.map(Document.parse))
     //val X2 = documents.toDF()
-    
+
     //val t =  docs.toDF()
     //val df = spark.read.format("json").json()
     //t.show()
     //val X = j.toDF()
-    //MongoSpark.save(X.write.option("collection", "DFClub").mode("append")) 
+    //MongoSpark.save(X.write.option("collection", "DFClub").mode("append"))
     //documents.toJavaRDD()
     //MongoSpark.save(documents)
-    //MongoSpark.save(centenarians.write.option("collection", "hundredClub").mode("overwrite")) 
-    
-      //sc.parallelize(docs.map(Document.parse)).saveToMongoDB()
-      
+    //MongoSpark.save(centenarians.write.option("collection", "hundredClub").mode("overwrite"))
+
+    //sc.parallelize(docs.map(Document.parse)).saveToMongoDB()
+
     //  sc.parallelize(docs.map(Document.parse))saveToMongoDB(WriteConfig(Map("uri" -> s"mongodb://127.0.0.1/myDBN.$collection")))
-      
-      
-      //documents.saveToMongoDB(WriteConfig(Map("uri" -> "mongodb://example.com/database.collection")))
-    
+
+    //documents.saveToMongoDB(WriteConfig(Map("uri" -> "mongodb://example.com/database.collection")))
 
   }
-  
-  //| Check your session Configurations 
-  def showConfigMap(){
-      val configMap:Map[String, String] = spark.conf.getAll
-      println(configMap)
-  }  
-  
+
+  //| Check your session Configurations
+  def showConfigMap() {
+    val configMap: Map[String, String] = spark.conf.getAll
+    println(configMap)
+  }
+
   //| Write to the DB
-  def writeToMongoDB(word:String,rank:String,rsclist:List[String]){
-      val rsc = rsclist.asJson
-      val x = s"""{"word":"$word","rank":"$rank","rsc":$rsc}"""
-      val doc = Document.parse(x)
-      val documents = sc.parallelize(Seq(doc))
-      MongoSpark.save(documents) 
-  }  
-  def writeFormedChunkToMongoDB(buffer:String,collection:String){
-      println("S2")
-      println(buffer)
-      val docs=buffer.trim.stripMargin.split("@#@").toSeq
-      println(docs)
-      println("S3")
-      sc.parallelize(docs.map(Document.parse)).saveToMongoDB(WriteConfig(Map("uri" -> s"mongodb://127.0.0.1/myDBN.$collection")))
+  def writeToMongoDB(word: String, rank: String, rsclist: List[String]) {
+    val rsc = rsclist.asJson
+    val x = s"""{"word":"$word","rank":"$rank","rsc":$rsc}"""
+    val doc = Document.parse(x)
+    val documents = sc.parallelize(Seq(doc))
+    MongoSpark.save(documents)
+  }
+  def writeFormedChunkToMongoDB(buffer: String, collection: String) {
+    println("S2")
+    println(buffer)
+    val docs = buffer.trim.stripMargin.split("@#@").toSeq
+    println(docs)
+    println("S3")
+    sc.parallelize(docs.map(Document.parse)).saveToMongoDB(WriteConfig(Map("uri" -> s"mongodb://127.0.0.1/myDBN.$collection")))
   }
 
-  def formRecord(id:Integer,word:String,rank:Double,rsclist:List[String]): String ={
-      val rsc = rsclist.asJson
-      val x = s"""{"_id":$id,"word":"$word","rank":$rank,"rsc":$rsc}"""
-      x
+  def formRecord(id: Integer, word: String, rank: Double, rsclist: List[String]): String = {
+    val rsc = rsclist.asJson
+    val x = s"""{"_id":$id,"word":"$word","rank":$rank,"rsc":$rsc}"""
+    x
   }
-  
+
   // Using the SQL helpers and StructFields helpers
-  def writeRecordToDB(){
-      val objectId = "123400000000000000000000"
-      val newDocs = Seq(new Document("_id", new ObjectId(objectId)).append("a", 1), new Document("_id", new ObjectId()).append("a", 2))
-      MongoSpark.save(sc.parallelize(newDocs))
-      /*
+  def writeRecordToDB() {
+    val objectId = "123400000000000000000000"
+    val newDocs = Seq(new Document("_id", new ObjectId(objectId)).append("a", 1), new Document("_id", new ObjectId()).append("a", 2))
+    MongoSpark.save(sc.parallelize(newDocs))
+    /*
        * val documents = sc.parallelize(
        *  Seq(new Document("fruits", List("apples", "oranges", "pears").asJava))
        * )
        */
   }
-  
-  def ChangeCollection(){
+
+  def ChangeCollection() {
     val characters = MongoSpark.load(spark)
     characters.createOrReplaceTempView("characters")
-    
+
     val centenarians = spark.sql("SELECT name, age FROM characters WHERE age >= 100")
     centenarians.show()
-    
+
     MongoSpark.save(centenarians.write.option("collection", "hundredClub").mode("overwrite")) // Append or overwrite <overwrite is buggy when the collection already exists>
 
     println("Reading from the 'hundredClub' collection:")
     MongoSpark.load(spark, ReadConfig(Map("collection" -> "hundredClub"), Some(ReadConfig(spark)))).show()
   }
-  
-  def FetchMaxId(collection: String) : Int = {
 
-    val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> s"mongodb://127.0.0.1/ArcanaDB.$collection" )))
+  def FetchMaxId(collection: String): Int = {
+
+    val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> s"mongodb://127.0.0.1/ArcanaDB.$collection")))
     rdd2.toDF().createOrReplaceTempView("DB")
     val maxID = spark.sql("SELECT max(cast(_id as int)) FROM DB")
 
-    maxID.collect()(0).getInt(0)   
-}
-  def readCollection(collection: String)  {
+    maxID.collect()(0).getInt(0)
+  }
+  def readCollection(collection: String) {
 
-    val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> s"mongodb://127.0.0.1/myDBN.$collection" )))
+    val rdd2 = sc.loadFromMongoDB(ReadConfig(Map("spark.mongodb.input.uri" -> s"mongodb://127.0.0.1/myDBN.$collection")))
     rdd2.toDF().createOrReplaceTempView("DB")
     rdd2.toDF().show
     val word = "nuclearbomb"
@@ -170,84 +166,109 @@ object AppDBM {
     res.show
     res.collect().foreach(println)
     //for (e <- res) println(e)
-}
-  
-  def EnterSchemaData(){
-    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+  }
+
+  def EnterSchemaData() {
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
-    
-    var z = Array[Integer](8,8,8)
-    
-    val days = List(1,2,3)
-    
-    val theRow =Row(33,"tito",days, Array[Double](1.3,1.3,1.3))
-    val theRow2 =Row(31,"dima",List(9,9,9), Array[Double](1.3,1.3,1.3))
+
+    var z = Array[Integer](8, 8, 8)
+
+    val days = List(1, 2, 3)
+
+    val theRow = Row(33, "tito", days, Array[Double](1.3, 1.3, 1.3))
+    val theRow2 = Row(31, "dima", List(9, 9, 9), Array[Double](1.3, 1.3, 1.3))
     //val theRow2 =Row(7,"dima",Array[java.lang.Integer](9,9,9), Array[Double](1.3,1.3,1.3))
-    val theRdd = sc.makeRDD(Array(theRow,theRow2))
-    
-    val df=theRdd.map{
-        case Row(s0,s1,s2,s3)=>X(s0.asInstanceOf[Int],s1.asInstanceOf[String],s2.asInstanceOf[List[Integer]],s3.asInstanceOf[Array[Double]])
-        }.toDF()
+    val theRdd = sc.makeRDD(Array(theRow, theRow2))
+
+    val df = theRdd.map {
+      case Row(s0, s1, s2, s3) => X(s0.asInstanceOf[Int], s1.asInstanceOf[String], s2.asInstanceOf[List[Integer]], s3.asInstanceOf[Array[Double]])
+    }.toDF()
     df.show()
 
     //military.show()
     MongoSpark.save(df.write.option("collection", "testcase").mode("append"))
   }
-  // Clean the subject and get back what it talks about 
-  def getExpFromSubject(Subject:String):String={
-    var temp = (Subject.substring(Subject.lastIndexOf('/')  + 1)).replaceAll(">","")      
-    temp = if (temp contains ':') temp.substring(temp.lastIndexOf(':')  + 1) else temp
+  // Clean the subject and get back what it talks about
+  def getExpFromSubject(Subject: String): String = {
+    var temp = (Subject.substring(Subject.lastIndexOf('/') + 1)).replaceAll(">", "")
+    temp = if (temp contains ':') temp.substring(temp.lastIndexOf(':') + 1) else temp
     temp
   }
-  
-  // Add the URIS that has the category in the URI to the database 
-  def addUrisBelongingToCategoryToDB(FileName:String){
-    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.implicits._
 
-    val DF=RDFApp.exportingData(FileName)
+  // Add the URIS that has the category in the subject to the database
+  def addUrisBelongingToCategoryToDB(FileName: String) {
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
+    var _idCounter: Int = 0
+    val DF = RDFApp.exportingData(FileName)
     
-    val category="war"
-    val myUriList = Dataset2Vec.fetchAllOfWordAsSubject(DF.toDF(),category)
-    var  _idCounter: Int=0
+    val categories = Categories.categories
+    val category = "war"
+    
     var DBRows = ArrayBuffer[Row]()
-    for(x<-myUriList){
-      DBRows+=Row(_idCounter , x.Uri, getExpFromSubject(x.Uri), category, 0.0 , 0.0)
-      _idCounter+=1
+    for ( x <- categories ){
+          println(x)
+          val myUriList = Dataset2Vec.fetchAllOfWordAsSubject(DF.toDF(), x)
+              for (y <- myUriList) {
+                DBRows += Row(_idCounter, y.Uri, getExpFromSubject(y.Uri), category, 0.0, 0.0, "")
+                _idCounter += 1
+              } 
     }
-    
+
     val dbRdd = sc.makeRDD(DBRows)
 
-    val df=dbRdd.map{
-        case Row(s0,s1,s2,s3,s4,s5)=>DBRecord(s0.asInstanceOf[Int],s1.asInstanceOf[String],s2.asInstanceOf[String],s3.asInstanceOf[String],s4.asInstanceOf[Double],s5.asInstanceOf[Double])
-        }.toDF()
-    MongoSpark.save(df.write.option("collection", DBConf.defaultCollection).mode("append"))    
+    val df = dbRdd.map {
+      case Row(s0, s1, s2, s3, s4, s5, s6) => DBRecord(s0.asInstanceOf[Int], s1.asInstanceOf[String], s2.asInstanceOf[String], s3.asInstanceOf[String], s4.asInstanceOf[Double], s5.asInstanceOf[Double], s6.asInstanceOf[String])
+    }.toDF()
+    MongoSpark.save(df.write.option("collection", DBConf.defaultCollection).mode("append"))
   }
-  //Schema 
-  case class X(_id: Int,_expression: String,indices: List[Integer], weights: Array[Double] )  
-  case class Record(_id: Int, expression: String, rank:Double, rsc: List[String])  
-  case class DBRecord(_id: Int, uri: String, expression: String, category: String, score:Double , weight:Double)  // score = cosine similary - weight = sentiment analysis
-  
+  //Schema
+  case class X(_id: Int, _expression: String, indices: List[Integer], weights: Array[Double])
+  case class Record(_id: Int, expression: String, rank: Double, rsc: List[String])
+
+  case class DBRecord(_id: Int, uri: String, expression: String, category: String, score: Double, weight: Double, objectOf: String) // score = cosine similary - weight = sentiment analysis
+
   def main(args: Array[String]) = {
     //> showConfigMap()
-    
+
     //> writeToMongoDB("ALIroops","3",List[String]("http://dbpedia.org/resource/Territorial_Troops1", "http://dbpedia.org/resource/Territorial_Troops2", "http://dbpedia.org/resource/Territorial_Troops3","http://dbpedia.org/resource/Territorial_Troops4","http://dbpedia.org/resource/Territorial_Troops5","http://dbpedia.org/resource/Territorial_Troops6","http://dbpedia.org/resource/Territorial_Troops7"))
-    
 
     //"src/main/resources/rdf2.nt"
     //val synonyms = model.findSynonyms("school",1000)
     //    val model=Word2VecModelMaker.loadWord2VecModel()
-    println(FetchMaxId("ArcanaTest"))
+    //println(FetchMaxId("ArcanaTest"))
+
+    
+    
+    //addUrisBelongingToCategoryToDB("src/main/resources/rdf2.nt")
+    val rdd =  MongoSpark.load(spark)
+
+      println(rdd.count)
+    //  println(rdd.first.toJson)
+    
+    rdd.createOrReplaceTempView("mongoDB")
+
+    val centenarians = spark.sql("SELECT * FROM mongoDB where ")
+    
+    val newT = spark.sql("update mongoDB set weight = '10' where expression = 'nuclearA1' ")
+
+    newT.show()
+
     
     //> writeChunkToMongoDB()
-    
+
     //> EnterSchemaData()
     //> println(FetchMaxId("testcase"))
-    
+
     //Writing a chunktto Mongo
     //> writeChunkToMongoDB("ChunkCase")
-    
-    
+
+    println("===================CLOSING===================")
+    spark.stop()
+  }
+}
+
     /*
     val buf = new ArrayBuffer[String]()
  
@@ -266,8 +287,3 @@ object AppDBM {
     readCollection("neuclear")
     
     */
-    println("===================CLOSING===================") 
-    spark.stop()
-  }
-}
-
