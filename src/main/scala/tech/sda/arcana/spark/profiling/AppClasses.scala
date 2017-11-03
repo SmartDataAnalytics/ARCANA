953320a66a63a072e4d76c569dbbf54eb0448a82
Ali Denno <aliedenno@gmail.com>
2017-11-03 10:37:37 +0100

    Create AppClasses
    A place to keep all the clases

--- src/main/scala/tech/sda/arcana/spark/profiling/AppClasses.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/AppClasses.scala
@@ -1,17 +1,18 @@
 package tech.sda.arcana.spark.profiling
-/*
- * A class that defines the malicious Categories that should be dealt with 
- */
 import scala.collection.mutable.ListBuffer
+
 class RDFURI(var Uri: String) {
    //var uris = new ListBuffer[RDFURI]()
    //var URIslist = List.newBuilder[URI]
    var URIslist  = List[RDFURI]()
    var FormedURI = ""
 }
+
 class Category (var Category: String,var uri:  List[RDFURI] ) {
    
   var FormedURI = ""
   //var categories = Array("military", "nuclear", "terrorism", "weapon", "technology", "security", "harm", "suicide")
 }
 
+  case class Synonym(word: String, similarity: Double)
+  case class DBRecord(_id: Int, uri: String, expression: String, category: String, score: Double, weight: Double, objectOf: String) // score = cosine similary - weight = sentiment analysis
