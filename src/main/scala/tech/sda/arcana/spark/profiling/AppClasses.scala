2eb3a33373a43ba67d5206586eede5d0a6863f38
Ali Denno <aliedenno@gmail.com>
2017-11-09 15:20:33 +0100

    Update AppClasses
    Moving more classes to this file

--- src/main/scala/tech/sda/arcana/spark/profiling/AppClasses.scala
+++ src/main/scala/tech/sda/arcana/spark/profiling/AppClasses.scala
@@ -9,11 +9,11 @@ class RDFURI(var Uri: String) {
 }
 
 class Category (var Category: String,var uri:  List[RDFURI] ) {
-   
   var FormedURI = ""
-  //var categories = Array("military", "nuclear", "terrorism", "weapon", "technology", "security", "harm", "suicide")
 }
-
+  case class X(_id: Int, _expression: String, indices: List[Integer], weights: Array[Double])
+  case class Record(_id: Int, expression: String, rank: Double, rsc: List[String])
+  case class Triple(Subject:String, Predicate:String, Object:String)
   case class Synonym(word: String, similarity: Double)
   case class DBRecord(_id: Int, uri: String, expression: String, category: String, score: Double, weight: Double, objectOf: String) // score = cosine similary - weight = sentiment analysis
   case class SentiWordNetClass(POS:String, ID:String, PosScore:String, NegScore:String, SynsetTerms:String)
