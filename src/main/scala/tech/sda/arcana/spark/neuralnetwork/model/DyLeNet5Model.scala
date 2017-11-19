01d1c65512afd91d950ceaa3cbc155cfad8ec4da
mhddenno <dennokhmhd@gmail.com>
2017-11-19 18:55:31 +0100

    update the Lenet model

--- src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
+++ src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
@@ -17,25 +17,26 @@ object DyLeNet5Model {
     // 1 input channel, 6 output channels, 5x5 convolution kernel
     LeNet5Model.add(SpatialConvolution(1, 6, 5, 5))//DenseTensor of size 1x6x1x5x5
     //non-linearity (+Z)
-    LeNet5Model.add(ReLU())
+    LeNet5Model.add(Tanh())
     //A max-pooling operation that looks at 2x2 windows and finds the max.
     LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
     // 6 input channel, 16 output channels, 5x5 convolution kernel
     LeNet5Model.add(SpatialConvolution(6,16,5,5))//DenseTensor of size 1x16x6x5x5
     //non-linearity (+Z)
-    LeNet5Model.add(ReLU())
+    LeNet5Model.add(Tanh())
     LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
     //reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
     LeNet5Model.add(Reshape(Array(view)))
     //fully connected layer (matrix multiplication between input and weights)
     LeNet5Model.add(Linear(view,120))
-    LeNet5Model.add(ReLU())
+    LeNet5Model.add(Tanh())
     LeNet5Model.add(Linear(120,84))
-    LeNet5Model.add(ReLU())
+    LeNet5Model.add(Tanh())
     LeNet5Model.add(Linear(84,10))
     LeNet5Model.add(LogSoftMax())
     //To get one result in the end
-    //LeNet5Model.add(Linear(10,1))
+    LeNet5Model.add(Linear(10,1))
+    LeNet5Model.add(Tanh())
     //LeNet5Model.add(LogSoftMax())
     //Printing the model specifications
     //println(LeNet5Model.getParametersTable())
