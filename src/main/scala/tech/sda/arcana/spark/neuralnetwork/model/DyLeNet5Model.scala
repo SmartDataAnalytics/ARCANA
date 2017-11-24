b8bb7292be02b1473f393484abf96994a3449e4f
mhddenno <dennokhmhd@gmail.com>
2017-11-24 10:40:26 +0100

    adding the class specification feature

--- src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
+++ src/main/scala/tech/sda/arcana/spark/neuralnetwork/model/DyLeNet5Model.scala
@@ -5,7 +5,7 @@ import com.intel.analytics.bigdl.nn._
 /** Object represents LeNetModel takes ([even#]>12 X [even#]>12) input size */
 object DyLeNet5Model {
   
-  def build(Height:Int,Width:Int)={
+  def build(Height:Int,Width:Int,classNum: Int)={
     //constraint to make sure that the input is valid 
     if((Height<=12 || Width<=12)||(Height%2!=0 || Width%2!=0)){
       val message:String="error"
@@ -15,27 +15,31 @@ object DyLeNet5Model {
     val view:Int=16*((((Height-4)/2)-4)/2)*((((Width-4)/2)-4)/2)
     val LeNet5Model= new Sequential()
     // 1 input channel, 6 output channels, 5x5 convolution kernel
-    LeNet5Model.add(SpatialConvolution(1, 6, 5, 5))//DenseTensor of size 1x6x1x5x5
-    //non-linearity (+Z)
+    LeNet5Model.add(SpatialConvolution(nInputPlane=1,nOutputPlane=6,kernelW=5,kernelH=5))//DenseTensor of size 1x6x1x5x5
+    //= (exp(x)-exp(-x))/(exp(x)+exp(-x))
     LeNet5Model.add(Tanh())
     //A max-pooling operation that looks at 2x2 windows and finds the max.
-    LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
+    //kW=kernel width,kH=kernel height,dW=step size in width,dH=step size in height
+    LeNet5Model.add(SpatialMaxPooling(kW=2,kH=2,dW=2,dH=2))
     // 6 input channel, 16 output channels, 5x5 convolution kernel
     LeNet5Model.add(SpatialConvolution(6,16,5,5))//DenseTensor of size 1x16x6x5x5
-    //non-linearity (+Z)
+    //= (exp(x)-exp(-x))/(exp(x)+exp(-x))
     LeNet5Model.add(Tanh())
     LeNet5Model.add(SpatialMaxPooling(2,2,2,2))
     //reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
     LeNet5Model.add(Reshape(Array(view)))
     //fully connected layer (matrix multiplication between input and weights)
     LeNet5Model.add(Linear(view,120))
+    //= (exp(x)-exp(-x))/(exp(x)+exp(-x))
     LeNet5Model.add(Tanh())
     LeNet5Model.add(Linear(120,84))
+    //= (exp(x)-exp(-x))/(exp(x)+exp(-x))
     LeNet5Model.add(Tanh())
     LeNet5Model.add(Linear(84,10))
     LeNet5Model.add(LogSoftMax())
     //To get one result in the end
-    LeNet5Model.add(Linear(10,1))
+    LeNet5Model.add(Linear(10,classNum))
+    //= (exp(x)-exp(-x))/(exp(x)+exp(-x))
     LeNet5Model.add(Tanh())
     //LeNet5Model.add(LogSoftMax())
     //Printing the model specifications
